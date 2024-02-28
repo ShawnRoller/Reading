b951599be73c6ce2e7d47086d7836afe81c95fde
Shawn Roller <offensivelybad@yahoo.com>
2024-02-27 20:10:16 -0500

    Initial Commit

---
+++ Reading/ReadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingApp.swift
+//  Reading
+//
+//  Created by Shawn Roller on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct ReadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
